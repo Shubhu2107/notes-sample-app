d8449aafc808c740072414318d577f323cb93472
sumit3108 <sumitchitkote3108@gmail.com>
2023-01-07 10:47:06 +0530

    app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+import { SandeepComponent } from './sandeep/sandeep.component';
+import { ShubhangiComponent } from './shubhangi/shubhangi.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'shubhangi', component: ShubhangiComponent },
+  { path: 'sandeep', component: SandeepComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
