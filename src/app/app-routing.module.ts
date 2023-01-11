3555a532bf527dbbe6e72bd8c2ceab9b05cc04cc
Sandeep Shradhye <shradhye@gmail.com>
2023-01-11 18:56:11 +0530

    routing done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
+import { HomeComponent } from './compo/home/home.component';
+import { CreateNewComponent } from './compo/create-new/create-new.component';
+import { SettingsComponent } from './compo/settings/settings.component';
+import { SearchComponent } from './compo/search/search.component';
+import { ColorComponentComponent } from './compo/color-component/color-component.component';
+import { CosmaticComponentComponent } from './compo/cosmatic-component/cosmatic-component.component';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'create-new.component', component: CreateNewComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'color-component.component', component: ColorComponentComponent },
+  { path: 'cosmatic-component.component', component: CosmaticComponentComponent },
 ];
 
 @NgModule({
